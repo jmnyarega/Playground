767bdd5fb63abbeac19c396cb505f55b1366edc8
jmnyarega <josiah.mokobo@andela.com>
2018-08-12 11:04:47 +0300

    👻 Implement middlewares 👀

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-// creating a simple server with node 
+var express = require('express');
+var { hello, log } = require('./middlewares');
+var app = express();
+var port = 8000;
 
-const http = require('http');
+/** You can use pass middlewares inside app.use() function 
+ * app.use(hello);
+ * Or pass it inside the app.get/post/delete/patch/update etc methods
+ */
 
-http.createServer(function(req, res) {
-    res.writeHeader(200);
-    res.write("Hello world");
-    res.end()
-}).listen(7000);
+app.get('/', hello, log, function(req, res) {
+    res.statusCode = 303;
+    res.json({
+        name: 20,
+    });
+});
+
+app.listen(port, function() {
+    console.log(`Server running on ${port}`);
+});
