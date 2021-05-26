ed97dfffa23851803f4651a89362122271718a36
josiahmokob0 <jmnyarega@gmail.com>
2021-05-26 19:58:44 +0300

    Factory Pattern

---
+++ Factory/Shape/Composition.ts
@@ -0,0 +1,39 @@
+/*
+ * circle = draw
+ * square = draw
+ * rectangle = draw
+ */
+
+const circle = () => ({
+  draw: () => console.log("This is a circle"),
+});
+
+const square = () => ({
+  draw: () => console.log("This is a square"),
+});
+
+const rectangle = () => ({
+  draw: () => console.log("This is a rectangle"),
+});
+
+const Factory = (shape: string) => ({
+  [shape]: shapes[shape],
+});
+
+const shapes = {
+  circle: circle(),
+  rectangle: rectangle(),
+  square: square(),
+};
+
+// circle
+const c = Factory("circle").circle;
+c.draw();
+
+// rectangle
+const r = Factory("rectangle").rectangle;
+r.draw();
+
+// square
+const s = Factory("square").square;
+s.draw();
